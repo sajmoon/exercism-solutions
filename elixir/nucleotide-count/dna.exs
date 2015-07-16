b02a861483c85acb50e14f8f62e4392d0e4f7160
Simon Ström <simon.strom@dewire.com>
2015-07-16 18:07:42 +0200

    elixir: nucleotide_count

---
+++ elixir/nucleotide-count/dna.exs
@@ -0,0 +1,45 @@
+defmodule DNA do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a DNA strand.
+
+  ## Examples
+
+  iex> DNA.count('AATAA', ?A)
+  4
+
+  iex> DNA.count('AATAA', ?T)
+  1
+  """
+  @spec count([char], char) :: non_neg_integer
+  def count(strand, nucleotide) do
+    count_letter(strand, nucleotide)
+  end
+
+  defp count_letter([], nucleotide), do: 0
+  defp count_letter(char, char), do: 1
+  defp count_letter([h|t], nucleotide) do
+    count(h, nucleotide) + count(t, nucleotide)
+  end
+  defp count_letter(strand, char), do: 0
+
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> DNA.nucleotide_counts('AATAA')
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec nucleotide_counts([char]) :: Dict.t
+  def nucleotide_counts(strand) do
+    %{
+      ?A => count(strand, ?A),
+      ?T => count(strand, ?T),
+      ?C => count(strand, ?C),
+      ?G => count(strand, ?G)
+    }
+  end
+end
